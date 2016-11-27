2fba3cd5593e6b98de4466eae976afe67d0f58e4
Devan Carlson <devansc@gmail.com>
2016-11-26 17:22:45 -0800

    Added landing page that shows bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,9 @@
 class Bookmark < ApplicationRecord
+    belongs_to :user
+
+    validates :user_id, presence: true
+
+    validates :title, length: {minimum: 10}, presence:true
+
+    validates :url, format: {with: Regexp.new(URI::regexp(%w(http https)))}, presence: true
 end
